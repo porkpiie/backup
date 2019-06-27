c955596a284a549cd7b578b4b8f0ccdd1c44550b
porkpiie <adtready@gmail.com>
2019-06-27 11:27:02 +0100

    added homepage method

--- src/main/java/com/qa/controller/HomeController.java
+++ src/main/java/com/qa/controller/HomeController.java
@@ -1,7 +1,13 @@
 package com.qa.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HomeController {
+
+    @RequestMapping("/") //this is for specifying the url that will then trigger this method.
+    public String home(){
+        return "Hiya Everybody";
+    }
 }
